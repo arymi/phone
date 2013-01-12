592808deebb573fa96a2843eeb99ac4475cf02d1
付占伦 <dx_fz@yahoo.cn>
2013-01-13 00:04:41 +0800

    增加 Phonegap支持

--- js/index.js
+++ js/index.js
@@ -1,65 +1,68 @@
-var _ = 'http://192.168.10.200:3000', channel;
+//配置参数
+var _ = {
+	"serverUrl": "http://192.168.10.200:3000",
+	"version": "0",
+	"channel": ""
+};
 
+//JM 初始化
 $(document).on('mobileinit', function(){
 	$.mobile.ajaxEnabled = false; //禁用自动ajax
 });
 
 $(document).ready(function(){
-	//版本比较
-	$.get(_ + '/v/' + localStorage.getItem('version'), function(data){
-		//与服务器版本相同
-		if(data.success){
-			$('#index_channel').append(localStorage.getItem('channel')).listview('refresh');
-		}else{
+	//版本初始化
+	$.ajax({
+		url: _.serverUrl + '/v/' + _.version,
+		type: 'GET',
+		dataType: 'json',
+		success: function(data){
+			$('#index_boxa').html(data.msg);
+			
+			//生成频道列表
 			var t = '';
 			$.each(data.channel, function(k, v){
 				t += '<li><a href="javascript:;" onclick="switchChannel(this)" data-channel="'+ v.ename +'">'+ v.cname +'</a></li>';
 			});
 			
-			//更新本地版本号
-			localStorage.removeItem('version');
-			localStorage.setItem('version', data.version);
-			
-			//更新本地频道项
-			localStorage.removeItem('channel');
-			localStorage.setItem('channel', t);
-			
+			$('#index_boxs').show();
 			$('#index_channel').append(t).listview('refresh');
+			
+			//next按钮绑定事件
+			$('#index_next').click(function(){
+				//切换显示区域
+				$('.index_box').toggle();
+				
+				//请求数据
+				$.get(_.serverUrl + '/r/' + _.channel, function(data){
+					$('.index_box:hidden').html(data.success ? data.items.detail: 'Error');
+				}, 'json');
+			});
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown){ //请求失败
+			$('#index_boxa').text('+_+ 抱歉，连接不到服务器...');
+			$('#index_boxs').show();
 		}
-		
-		//next按钮绑定事件
-		$('#index_next').click(function(){
-			//当前频道
-			if(!channel) return;
-			
-			//切换显示区域
-			$('.index_box').toggle();
-			//$('.index_box:hidden').text('Loading...');
-			
-			//请求数据
-			$.get(_ + '/r/' + channel, function(data){
-				$('.index_box:hidden').html(data.success ? data.items.detail: 'Error');
-			}, 'json');
-		});
-	}, 'json');
+	});
 });
 
 //切换频道
 function switchChannel(t){
-	//取得频道标记
 	var dc = t.getAttribute('data-channel');
-	if(dc == channel) return;
+	if(_.channel == dc) return; //避免重复选择
+	if(_.channel == '') $('#index_tool').show(); //第一次选择显示控制工具栏
+	
 	$('#index_title').text('Loading...');
 	
-	//请求数据
-	$.get(_ + '/c/' + dc, function(data){
+	//请求数据预载两条数据
+	$.get(_.serverUrl + '/c/' + dc, function(data){
 		if(data.success){
 			$('#index_boxa').html(data.items[0].detail);
 			$('#index_boxb').html(data.items[1].detail);
 			$('#index_title').text(t.text);
 			
 			//记录所选频道
-			channel = dc;
+			_.channel = dc;
 		}else{
 			$('#index_title').text('Error');
 		}
