f5bf936bdc5056d56ba233eb96fc7c5fb6d139b0
付占伦 <dx_fz@yahoo.cn>
2013-01-31 16:59:53 +0800

    测试phonegap 连接sqlite数据库

--- js/index.js
+++ js/index.js
@@ -1,70 +1,61 @@
-//配置参数
-var _ = {
-	"serverUrl": "http://192.168.10.200:3000",
-	"version": "0",
-	"channel": ""
-};
-
 //JM 初始化
 $(document).on('mobileinit', function(){
 	$.mobile.ajaxEnabled = false; //禁用自动ajax
 });
 
 $(document).ready(function(){
-	//版本初始化
-	$.ajax({
-		url: _.serverUrl + '/v/' + _.version,
-		type: 'GET',
-		dataType: 'json',
-		success: function(data){
-			$('#index_boxa').html(data.msg);
-			
-			//生成频道列表
-			var t = '';
-			$.each(data.channel, function(k, v){
-				t += '<li><a href="javascript:;" onclick="switchChannel(this)" data-channel="'+ v.ename +'">'+ v.cname +'</a></li>';
-			});
-			
-			$('#index_boxs').show();
-			$('#index_channel').append(t).listview('refresh');
-			
-			//next按钮绑定事件
-			$('#index_next').click(function(){
-				//切换显示区域
-				$('.index_box').toggle();
-				
-				//请求数据
-				$.get(_.serverUrl + '/r/' + _.channel, function(data){
-					$('.index_box:hidden').html(data.success ? data.items.detail: 'Error');
-				}, 'json');
-			});
-		},
-		error: function(XMLHttpRequest, textStatus, errorThrown){ //请求失败
-			$('#index_boxa').text('+_+ 抱歉，连接不到服务器...');
-			$('#index_boxs').show();
+	//搜索关键词
+	var kw = '';
+
+	//连接数据库
+	var db = window.openDatabase('test', '1.0', 'Test DB', 2000000);
+
+	//搜索
+	$('#index_search').live('keyup', function(e){
+		if(this.value != kw){
+			kw = this.value;
+			alert(kw);
+		}else{
+			return false;
 		}
+	});
+
+	//生成数据库
+	$('#index_cdb').on('click', function(e){
+		db.transaction(populateDB, errorCB, successCB);
+	});
+
+	//读取记录
+	$('#index_rdb').on('click', function(e){
+		 db.transaction(queryDB, errorCB);
 	});
 });
 
-//切换频道
-function switchChannel(t){
-	var dc = t.getAttribute('data-channel');
-	if(_.channel == dc) return; //避免重复选择
-	if(_.channel == '') $('#index_tool').show(); //第一次选择显示控制工具栏
-	
-	$('#index_title').text('Loading...');
-	
-	//请求数据预载两条数据
-	$.get(_.serverUrl + '/c/' + dc, function(data){
-		if(data.success){
-			$('#index_boxa').html(data.items[0].detail);
-			$('#index_boxb').html(data.items[1].detail);
-			$('#index_title').text(t.text);
-			
-			//记录所选频道
-			_.channel = dc;
-		}else{
-			$('#index_title').text('Error');
-		}
-	}, 'json');
+//错误回调
+function errorCB(err){
+	alert("Error processing SQL: " + err.code);
+}
+
+//成功回调
+function successCB(){
+	alert("success!");
+}
+
+//执行SQL
+function populateDB(tx){
+	tx.executeSql('DROP TABLE IF EXISTS DEMO');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "First row")');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
+	tx.executeSql('INSERT INTO DEMO (id, data) VALUES (3, "Three row")');
+}
+
+//查询
+function queryDB(tx){
+	tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+}
+
+//查询成功
+function querySuccess(tx, results){
+	alert(results.rows.length);
 }
